a4a84c51b3418cd867bd99bb7d958b6f727de015
dmitriiyyyy <dmitriyscorpio@gmail.com>
2025-05-20 10:51:35 -0700

    upd

--- VisualNTSystem/VisualNTSystem/InnerWindow.h
+++ VisualNTSystem/VisualNTSystem/InnerWindow.h
@@ -46,6 +46,10 @@ namespace VisualNTSystem {
 				delete components;
 			}
 		}
+	private: System::Windows::Forms::PictureBox^ toolboxBackground;
+	private: System::Windows::Forms::Label^ objects;
+	private: System::Windows::Forms::PictureBox^ canvasHeader;
+	protected:
 
 	private:
 		/// <summary>
@@ -60,17 +64,61 @@ namespace VisualNTSystem {
 		/// </summary>
 		void InitializeComponent(void)
 		{
+			System::ComponentModel::ComponentResourceManager^ resources = (gcnew System::ComponentModel::ComponentResourceManager(InnerWindow::typeid));
+			this->toolboxBackground = (gcnew System::Windows::Forms::PictureBox());
+			this->objects = (gcnew System::Windows::Forms::Label());
+			this->canvasHeader = (gcnew System::Windows::Forms::PictureBox());
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->toolboxBackground))->BeginInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->canvasHeader))->BeginInit();
 			this->SuspendLayout();
+			// 
+			// toolboxBackground
+			// 
+			this->toolboxBackground->Image = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"toolboxBackground.Image")));
+			this->toolboxBackground->Location = System::Drawing::Point(-1, 1);
+			this->toolboxBackground->Name = L"toolboxBackground";
+			this->toolboxBackground->Size = System::Drawing::Size(200, 640);
+			this->toolboxBackground->TabIndex = 1;
+			this->toolboxBackground->TabStop = false;
+			// 
+			// objects
+			// 
+			this->objects->AutoSize = true;
+			this->objects->BackColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(28)), static_cast<System::Int32>(static_cast<System::Byte>(44)),
+				static_cast<System::Int32>(static_cast<System::Byte>(54)));
+			this->objects->Font = (gcnew System::Drawing::Font(L"Candara", 15.75F));
+			this->objects->ForeColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(246)), static_cast<System::Int32>(static_cast<System::Byte>(246)),
+				static_cast<System::Int32>(static_cast<System::Byte>(246)));
+			this->objects->Location = System::Drawing::Point(59, 9);
+			this->objects->Name = L"objects";
+			this->objects->Size = System::Drawing::Size(81, 26);
+			this->objects->TabIndex = 5;
+			this->objects->Text = L"Objects";
+			// 
+			// canvasHeader
+			// 
+			this->canvasHeader->Image = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"canvasHeader.Image")));
+			this->canvasHeader->Location = System::Drawing::Point(-1, 1);
+			this->canvasHeader->Name = L"canvasHeader";
+			this->canvasHeader->Size = System::Drawing::Size(960, 50);
+			this->canvasHeader->TabIndex = 4;
+			this->canvasHeader->TabStop = false;
 			// 
 			// InnerWindow
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
-			this->ClientSize = System::Drawing::Size(284, 261);
+			this->ClientSize = System::Drawing::Size(960, 640);
+			this->Controls->Add(this->objects);
+			this->Controls->Add(this->canvasHeader);
+			this->Controls->Add(this->toolboxBackground);
 			this->Name = L"InnerWindow";
 			this->Text = L"InnerWindow";
 			this->Load += gcnew System::EventHandler(this, &InnerWindow::InnerWindow_Load);
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->toolboxBackground))->EndInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->canvasHeader))->EndInit();
 			this->ResumeLayout(false);
+			this->PerformLayout();
 
 		}
 #pragma endregion
